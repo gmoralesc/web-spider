b8c4224c6d9183757fb05e4760f759d735e89819
Gustavo Morales <gustavo.morales@morningstar.com>
2017-03-04 21:30:17 -0500

    refactor(avoid callback hell): refactor
    refactor main function to avoid callback hell
    
    BREAKING CHANGE: update .gitignore to add .DS_store files for Mac

--- index.js
+++ index.js
@@ -6,33 +6,44 @@ const mkdirp = require("mkdirp");
 const path = require("path");
 const utilities = require("./utilities");
 
+function saveFile(filename, contents, callback) {
+    mkdirp(path.dirname(filename), err => {
+        if (err) {
+            return callback(err);
+        }
+        fs.writeFile(filename, contents, callback);
+    });
+}
+
+function download(url, filename, callback) {
+    console.log(`Downloading ${url}`);
+    request(url, (err, response, body) => {
+        if (err) {
+            return callback(err);
+        }
+        saveFile(filename, body, err => {
+            if (err) {
+                return callback(err);
+            }
+            console.log(`Downloaded and saved: ${url}`);
+            callback(null, body);
+        });
+    });
+}
+
+
 function spider(url, callback){
     const filename = utilities.urlToFilename(url);
     fs.exists(filename, exists => {
-        if(!exists){
-            console.log(`Downloading ${url}`);
-            request(url, (err, response, body) => {
-                if(err){
-                    callback(err);
-                }else{
-                    mkdirp(path.dirname(filename), err => {
-                        if(err){
-                            callback(err);
-                        }else{
-                            fs.writeFile(filename, body, err => {
-                                if(err){
-                                    callback(err);
-                                }else{
-                                    callback(null, filename, true);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }else{
-            callback(null, filename, false);
+        if(exists){
+            return callback(null, filename, false);
         }
+        download(url, filename, err => {
+            if(err){
+                return callback(err);
+            }
+            callback(null, filename, true);
+        });
     });
 }
 
