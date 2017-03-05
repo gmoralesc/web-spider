9c67929fc2b0b0f42b5c08058f3ff9520bc0c10c
Gustavo Morales <gustavo.morales@morningstar.com>
2017-03-04 21:10:11 -0500

    feat: web spider
    a web spider to download the index page of a provided website

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 "use strict";
+
+const request = require("request");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const utilities = require("./utilities");
+
+function spider(url, callback){
+    const filename = utilities.urlToFilename(url);
+    fs.exists(filename, exists => {
+        if(!exists){
+            console.log(`Downloading ${url}`);
+            request(url, (err, response, body) => {
+                if(err){
+                    callback(err);
+                }else{
+                    mkdirp(path.dirname(filename), err => {
+                        if(err){
+                            callback(err);
+                        }else{
+                            fs.writeFile(filename, body, err => {
+                                if(err){
+                                    callback(err);
+                                }else{
+                                    callback(null, filename, true);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }else{
+            callback(null, filename, false);
+        }
+    });
+}
+
+spider(process.argv[2], (err, filename, downloaded) => {
+     if(err) {
+       console.log(err);
+     } else if(downloaded){
+       console.log(`Completed the download of "${filename}"`);
+     } else {
+       console.log(`"${filename}" was already downloaded`);
+     }
+});
